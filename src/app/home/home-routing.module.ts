0237e15b54a2eb1d03710ed942b847a17b7210a1
shivani743 <53550692+shivani743@users.noreply.github.com>
2022-01-13 18:56:25 +0530

    Add header footer

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+    // redirectTo: 'home',
+    // pathMatch: 'full',
+  },
+//   {
+//     path: 'home',
+//     component: HomeComponent,
+//     loadChildren: () => import('./home.module').then(m => m.HomeModule)
+    
+//   }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
