652aeb93f0c5d9c5ccec6e05e7359ad2fa94a940
shivani743 <53550692+shivani743@users.noreply.github.com>
2022-01-25 16:14:47 +0530

    css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -20,8 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClient,
-    // HomeComponent
+    HomeComponent
   ],
   declarations: [
     AppComponent,
